aff477decafd29d3df7612b5149e714b303c8171
Alex Wendland <me@alexwendland.com>
2013-11-21 23:54:18 -0800

    Increased errors and code functionality

---
+++ meeting 5/project-bad_code/ClassOne.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class ClassOne here.
+ *  
+ * @version 1
+ */
+public class ClassOne;
+{
+    // instance variables
+    var int mVar;
+
+    /**
+     * Constructor for objects of class ClassOne
+     */
+    public void ClassOne(); {
+    {
+        // initialise instance variables
+        mVar == 5.0;
+		
+		// call method and print result
+		System.out.println( double add(mVar, "6") );
+		
+		System-out-println( 5.2 / 2.3 );
+		// Take the remainder of 27 divided by 8
+		 * then add 6 to that, and finally divide the
+		 * whole thing by 3 */
+		System.out.println( 27 % 8 + 6 / 3 );
+    }
+
+    /**
+     * An example of a method
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int add(integer y, integer x);
+    [
+        // put your code here
+        return = x + y
+    ]
+}
